00214c9f74e4358c341a78e03e238fad1281bd85
Pavan-Rana <pavan.rana@kcl.ac.uk>
2022-12-02 00:29:54 +0000

    DONE

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,13 @@
 """Forms of the project."""
 
 # Create your forms here.
+from django import forms
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class ThingForm(forms.Form):
+    name = forms.CharField(label='name', max_length=35)
+    description = forms.CharField(label='description',widget=forms.Textarea(), max_length=120)
+    quantity = forms.IntegerField(label='quantity',widget=forms.NumberInput(),validators=[
+            MaxValueValidator(50),
+            MinValueValidator(0)
+        ])
